fd2b60178e0cf329bf41d74a6ae802f9ab5be428
cahaberly <cahaberly@gmail.com>
2016-03-10 12:14:33 -0500

    This is a new commit

---
+++ KnightExchange/Controllers/userController.vb
@@ -0,0 +1,108 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.Entity
+Imports System.Linq
+Imports System.Net
+Imports System.Web
+Imports System.Web.Mvc
+
+Namespace KnightExchange
+    Public Class userController
+        Inherits System.Web.Mvc.Controller
+
+        Private db As New KnightExchangeDBEntities
+
+        ' GET: /user/
+        Function Index() As ActionResult
+            Return View(db.users.ToList())
+        End Function
+
+        ' GET: /user/Details/5
+        Function Details(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim user As user = db.users.Find(id)
+            If IsNothing(user) Then
+                Return HttpNotFound()
+            End If
+            Return View(user)
+        End Function
+
+        ' GET: /user/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: /user/Create
+        'To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        'more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        <HttpPost()>
+        <ValidateAntiForgeryToken()>
+        Function Create(<Bind(Include := "user_number,user_fname,user_lname,user_email,user_phone,user_level,user_pswd,auth_level,auth_desc")> ByVal user As user) As ActionResult
+            If ModelState.IsValid Then
+                db.users.Add(user)
+                db.SaveChanges()
+                Return RedirectToAction("Index")
+            End If 
+            Return View(user)
+        End Function
+
+        ' GET: /user/Edit/5
+        Function Edit(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim user As user = db.users.Find(id)
+            If IsNothing(user) Then
+                Return HttpNotFound()
+            End If
+            Return View(user)
+        End Function
+
+        ' POST: /user/Edit/5
+        'To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        'more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        <HttpPost()>
+        <ValidateAntiForgeryToken()>
+        Function Edit(<Bind(Include := "user_number,user_fname,user_lname,user_email,user_phone,user_level,user_pswd,auth_level,auth_desc")> ByVal user As user) As ActionResult
+            If ModelState.IsValid Then
+                db.Entry(user).State = EntityState.Modified
+                db.SaveChanges()
+                Return RedirectToAction("Index")
+            End If
+            Return View(user)
+        End Function
+
+        ' GET: /user/Delete/5
+        Function Delete(ByVal id As Integer?) As ActionResult
+            If IsNothing(id) Then
+                Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            Dim user As user = db.users.Find(id)
+            If IsNothing(user) Then
+                Return HttpNotFound()
+            End If
+            Return View(user)
+        End Function
+
+        ' POST: /user/Delete/5
+        <HttpPost()>
+        <ActionName("Delete")>
+        <ValidateAntiForgeryToken()>
+        Function DeleteConfirmed(ByVal id As Integer) As ActionResult
+            Dim user As user = db.users.Find(id)
+            db.users.Remove(user)
+            db.SaveChanges()
+            Return RedirectToAction("Index")
+        End Function
+
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If (disposing) Then
+                db.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+    End Class
+End Namespace
---
+++ KnightExchange/Views/user/Edit.vbhtml
@@ -0,0 +1,95 @@
+﻿@ModelType user
+@Code
+    ViewData("Title") = "Edit"
+End Code
+
+<h2>Edit</h2>
+
+@Using (Html.BeginForm())
+    @Html.AntiForgeryToken()
+    
+    @<div class="form-horizontal">
+        <h4>user</h4>
+        <hr />
+        @Html.ValidationSummary(true)
+        @Html.HiddenFor(Function(model) model.user_number)
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_fname, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_fname)
+                @Html.ValidationMessageFor(Function(model) model.user_fname)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_lname, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_lname)
+                @Html.ValidationMessageFor(Function(model) model.user_lname)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_email, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_email)
+                @Html.ValidationMessageFor(Function(model) model.user_email)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_phone, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_phone)
+                @Html.ValidationMessageFor(Function(model) model.user_phone)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_level, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_level)
+                @Html.ValidationMessageFor(Function(model) model.user_level)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.user_pswd, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.user_pswd)
+                @Html.ValidationMessageFor(Function(model) model.user_pswd)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.auth_level, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.auth_level)
+                @Html.ValidationMessageFor(Function(model) model.auth_level)
+            </div>
+        </div>
+
+        <div class="form-group">
+            @Html.LabelFor(Function(model) model.auth_desc, New With { .class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EditorFor(Function(model) model.auth_desc)
+                @Html.ValidationMessageFor(Function(model) model.auth_desc)
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <input type="submit" value="Save" class="btn btn-default" />
+            </div>
+        </div>
+    </div>
+End Using
+
+<div>
+    @Html.ActionLink("Back to List", "Index")
+</div>
+
+@Section Scripts 
+    @Scripts.Render("~/bundles/jqueryval")
+End Section
